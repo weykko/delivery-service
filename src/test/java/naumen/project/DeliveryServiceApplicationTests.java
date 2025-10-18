4927e5fb6e22c59377b15cf7a42e46adc2caa9da
aleksandr beznutrov <king.gi410@gmail.com>
2025-10-18 21:19:00 +0500

    Initial commit

---
+++ src/test/java/naumen/project/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package naumen.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
