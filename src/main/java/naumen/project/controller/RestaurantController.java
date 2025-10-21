514f65e67c9ffe81705610c4d09605697f60b167
aleksandr <beznutrovs@gmail.com>
2025-10-22 00:39:55 +0500

    добавлена валидация

--- src/main/java/naumen/project/controller/RestaurantController.java
+++ src/main/java/naumen/project/controller/RestaurantController.java
@@ -1,5 +1,6 @@
 package naumen.project.controller;
 
+import jakarta.validation.Valid;
 import naumen.project.dto.menu.MenuItemRequestDto;
 import naumen.project.dto.menu.MenuItemResponseDto;
 import naumen.project.service.MenuService;
@@ -19,13 +20,13 @@ public class RestaurantController {
     //TODO: Блюдо должно создаваться для авторизованого ресторана, переделать после добавления авторизации
     @PostMapping("/menu")
     @ResponseStatus(HttpStatus.CREATED)
-    public MenuItemResponseDto createMenuItem(@RequestBody MenuItemRequestDto menuItemRequestDto) {
+    public MenuItemResponseDto createMenuItem(@RequestBody @Valid MenuItemRequestDto menuItemRequestDto) {
         return menuService.createMenuItem(menuItemRequestDto);
     }
 
     @PutMapping("/menu/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public MenuItemResponseDto updateMenuItem(@RequestBody MenuItemRequestDto menuItemRequestDto,
+    public MenuItemResponseDto updateMenuItem(@RequestBody @Valid MenuItemRequestDto menuItemRequestDto,
                                               @PathVariable Long id) {
         return menuService.updateMenuItem(id,  menuItemRequestDto);
     }
--- src/main/java/naumen/project/mapper/MenuMapper.java
+++ src/main/java/naumen/project/mapper/MenuMapper.java
@@ -3,6 +3,7 @@ package naumen.project.mapper;
 import naumen.project.dto.menu.MenuItemRequestDto;
 import naumen.project.dto.menu.MenuItemResponseDto;
 import naumen.project.entity.MenuItem;
+import naumen.project.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -14,6 +15,8 @@ public interface MenuMapper {
     @Mapping(target = "restaurant", ignore = true)
     MenuItem toEntity(MenuItemRequestDto request);
 
+    @Mapping(target = "restaurantId",
+            expression = "java(menuItem.getRestaurant() != null ? menuItem.getRestaurant().getId() : null)")
     MenuItemResponseDto toResponse(MenuItem menuItem);
 
     @Mapping(target = "id", ignore = true)
